51028f59ae281d15367e31bf6eec911c93a151c6
endless-online.com <admin@endless-online.com>
2023-09-18 13:00:00 +0000

    Updated 2023-09-18 at 13:00

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);
 }
